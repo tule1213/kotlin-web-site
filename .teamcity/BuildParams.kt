fe377a9d71853e6845edb714a2fc29f3ef34b889
zoobestik <kb.chernenko@gmail.com>
2024-09-23 18:14:25 +0200

    tmp; staging search index

--- .teamcity/BuildParams.kt
+++ .teamcity/BuildParams.kt
@@ -8,5 +8,5 @@ object BuildParams {
   const val KOTLIN_RELEASE_TAG = "v2.0.20"
 
   const val SEARCH_APP_ID = "7961PKYRXV"
-  const val SEARCH_INDEX_NAME = "prod_KOTLINLANG_WEBHELP"
+  const val SEARCH_INDEX_NAME = "stage_kotlinlang_webhelp"
 }
